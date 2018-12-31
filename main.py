3ee8e31cf13e94571b5e6b0c84eefc906c037e94
Victor Le <victor.le@protonmail.com>
2018-12-31 11:37:11 +0100

    Main revert to 2D

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from pyvox.models import Vox
-from pyvox.writer import VoxWriter
 
 from display import load_sample, show
 from wfc import WaveFunctionCollapse
 
 if __name__ == '__main__':
 
-    grid_size = (10, 10, 10)
-    pattern_size = (2, 2, 2)
+    grid_size = (1, 30, 30)
+    pattern_size = (1, 2, 2)
 
-    # sample = load_sample('samples/red_maze.png')
-    sample = load_sample('samples/test.vox')
+    sample = load_sample('samples/red_maze.png')
 
     wfc = WaveFunctionCollapse(grid_size, sample, pattern_size)
-
-    # wfc.run()
 
     fig, ax = plt.subplots()
     image = wfc.get_image()
@@ -33,16 +28,5 @@ if __name__ == '__main__':
 
         fig.canvas.draw()
         plt.pause(0.001)
-
-    print("image shape:", image.shape)
-    if len(image.shape) == 4:
-        print("-----------------")
-        image = wfc.get_image()
-        image = np.squeeze(image, axis=3)
-        image = image.astype(int)
-        print(image)
-        print(image.shape)
-        vox = Vox.from_dense(image)
-        VoxWriter('output.vox', vox).write()
 
     plt.show()

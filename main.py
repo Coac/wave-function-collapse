993fbba560a29073b64f887655345dc40bc34074
Victor Le <victor.le@protonmail.com>
2019-01-05 11:48:14 +0100

    Remove display file and Add blue sample

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ An example of using the wave function collapse with 2D image.
 import matplotlib.pyplot as plt
 import numpy as np
 
-from display import load_sample, show
 from wfc import WaveFunctionCollapse
 
 
@@ -24,15 +23,36 @@ def plot_patterns(patterns, title=''):
     plt.show()
 
 
+def load_sample(path):
+    sample = plt.imread(path)
+    # Expand dim to 3D
+    sample = np.expand_dims(sample, axis=0)
+    sample = sample[:, :, :, :3]
+
+    return sample
+
+
+def show(image):
+    if image.shape[0] == 1:
+        return plt.imshow(np.squeeze(image, axis=0))
+
+
 if __name__ == '__main__':
 
     grid_size = (1, 30, 30)
     pattern_size = (1, 2, 2)
 
-    sample = load_sample('samples/red_maze.png')
+    sample = load_sample('samples/blue.png')
+    show(sample)
+    plt.show()
 
     wfc = WaveFunctionCollapse(grid_size, sample, pattern_size)
     plot_patterns(wfc.get_patterns(), 'patterns')
+
+    # _, _, legal_patterns = wfc.propagator.legal_patterns(wfc.patterns[2], (0, 0, 1))
+    # show(Pattern.from_index(2).to_image())
+    # plt.show()
+    # plot_patterns([Pattern.from_index(i).to_image() for i in legal_patterns])
 
     fig, ax = plt.subplots()
     image = wfc.get_image()
